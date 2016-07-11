e4bff4de593fe8c6600bda1ea60e3f1da6536926
Christopher Bonuel <bonuelc@gmail.com>
2016-07-11 13:36:38 -0600

    Add Results storyboard view and controller

---
+++ BoutTime/ResultsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ResultsViewController.swift
+//  BoutTime
+//
+//  Created by Christopher Bonuel on 7/11/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}

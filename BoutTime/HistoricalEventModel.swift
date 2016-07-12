6ad77881ae24dd0cd41130397fa0d84667823a45
Christopher Bonuel <bonuelc@gmail.com>
2016-07-11 19:20:55 -0600

    Add website attribute to Event struct to use as a link in game

--- BoutTime/HistoricalEventModel.swift
+++ BoutTime/HistoricalEventModel.swift
@@ -24,60 +24,36 @@ struct Event: Equatable, Comparable {
     let description: String
     let year: Int
     let month: Int
+    let website: String
 }
 
 struct HistoricalEventModel {
     let eventCollection: [Event] = [
-        // https://en.wikipedia.org/wiki/History_of_Python
-        Event(description: "Python 1.0 release", year: 1994, month: 1),
-        // https://en.wikipedia.org/wiki/Swift_(programming_language)
-        Event(description: "Swift 1.0 release", year: 2014, month: 9),
-        // https://en.wikipedia.org/wiki/Michael_Jordan
-        Event(description: "Michael Jordan wins his first NBA championship", year: 1991, month: 6),
-        // https://en.wikipedia.org/wiki/Moon_landing
-        Event(description: "Neil Armstrong walks on the moon", year: 1969, month: 7),
-        // https://en.wikipedia.org/wiki/Cheers
-        Event(description: "Cheers sitcom first aires", year: 1982, month: 9),
-        // https://en.wikipedia.org/wiki/Post-it_note
-        Event(description: "Post-it's are first sold across the US", year: 1980, month: 4),
-        // https://en.wikipedia.org/wiki/World_War_I
-        Event(description: "World War I starts", year: 1914, month: 7),
-        // https://en.wikipedia.org/wiki/World_War_II
-        Event(description: "World War II starts", year: 1939, month: 9),
-        // https://en.wikipedia.org/wiki/Kleenex
-        Event(description: "Kleenex facial tissue appears on market", year: 1924, month: 1),
-        // https://en.wikipedia.org/wiki/To_Kill_a_Mockingbird
-        Event(description: "To Kill a Mockingbird published", year: 1960, month: 7),
-        // https://en.wikipedia.org/wiki/The_Beatles
-        Event(description: "The Beatles are formed", year: 1960, month: 1),
-        // https://en.wikipedia.org/wiki/Color_television
-        Event(description: "First national TV broadcast in color", year: 1954, month: 1),
-        // https://en.wikipedia.org/wiki/John_F._Kennedy
-        Event(description: "JFK becomes president", year: 1961, month: 1),
-        // https://en.wikipedia.org/wiki/Wall_Street_Crash_of_1929
-        Event(description: "Black Tuesday", year: 1929, month: 10),
-        // https://en.wikipedia.org/wiki/Rose_Bowl_Game
-        Event(description: "First Rose Bowl game played", year: 1902, month: 1),
-        // https://en.wikipedia.org/wiki/Ford_Model_T
-        Event(description: "Ford Model T appears on market", year: 1908, month: 10),
-        // https://en.wikipedia.org/wiki/Girl_Scouts_of_the_USA
-        Event(description: "Girl Scouts are formed", year: 1912, month: 3),
-        // https://en.wikipedia.org/wiki/Mickey_Mouse
-        Event(description: "Mickey Mouse's first appearance", year: 1928, month: 11),
-        // https://en.wikipedia.org/wiki/Golden_Gate_Bridge
-        Event(description: "Golden Gate Bridge completed in San Francisco", year: 1937, month: 4),
-        // https://en.wikipedia.org/wiki/Iron_Curtain
-        Event(description: "Winston Churchill's Iron Curtain speech", year: 1946, month: 3),
-        // https://en.wikipedia.org/wiki/Peanuts
-        Event(description: "The comic strip Peanuts is first published", year: 1950, month: 10),
-        // https://en.wikipedia.org/wiki/Today_(U.S._TV_program)
-        Event(description: "The debut of the Today show on NBC", year: 1952, month: 1),
-        // https://en.wikipedia.org/wiki/Disneyland
-        Event(description: "Disneyland opens in California", year: 1955, month: 7),
-        // https://en.wikipedia.org/wiki/Sesame_Street
-        Event(description: "Sesame Street premieres on TV", year: 1969, month: 11),
-        // https://en.wikipedia.org/wiki/Earth_Day
-        Event(description: "The first Earth Day is observed", year: 1970, month: 4)
+        Event(description: "Python 1.0 release", year: 1994, month: 1, website: "https://en.wikipedia.org/wiki/History_of_Python"),
+        Event(description: "Swift 1.0 release", year: 2014, month: 9, website: "https://en.wikipedia.org/wiki/Swift_(programming_language)"),
+        Event(description: "Michael Jordan wins his first NBA championship", year: 1991, month: 6, website: "https://en.wikipedia.org/wiki/Michael_Jordan"),
+        Event(description: "Neil Armstrong walks on the moon", year: 1969, month: 7, website: "https://en.wikipedia.org/wiki/Moon_landing"),
+        Event(description: "Cheers sitcom first aires", year: 1982, month: 9, website: "https://en.wikipedia.org/wiki/Cheers"),
+        Event(description: "Post-it's are first sold across the US", year: 1980, month: 4, website:  "https://en.wikipedia.org/wiki/Post-it_note"),
+        Event(description: "World War I starts", year: 1914, month: 7, website: "https://en.wikipedia.org/wiki/World_War_I"),
+        Event(description: "World War II starts", year: 1939, month: 9, website: "https://en.wikipedia.org/wiki/World_War_II"),
+        Event(description: "Kleenex facial tissue appears on market", year: 1924, month: 1, website: "https://en.wikipedia.org/wiki/Kleenex"),
+        Event(description: "To Kill a Mockingbird published", year: 1960, month: 7, website: "https://en.wikipedia.org/wiki/To_Kill_a_Mockingbird"),
+        Event(description: "The Beatles are formed", year: 1960, month: 1, website: "https://en.wikipedia.org/wiki/The_Beatles"),
+        Event(description: "First national TV broadcast in color", year: 1954, month: 1, website: "https://en.wikipedia.org/wiki/Color_television"),
+        Event(description: "JFK becomes president", year: 1961, month: 1, website: "https://en.wikipedia.org/wiki/John_F._Kennedy"),
+        Event(description: "Black Tuesday", year: 1929, month: 10, website: "https://en.wikipedia.org/wiki/Wall_Street_Crash_of_1929"),
+        Event(description: "First Rose Bowl game played", year: 1902, month: 1, website: "https://en.wikipedia.org/wiki/Rose_Bowl_Game"),
+        Event(description: "Ford Model T appears on market", year: 1908, month: 10, website: "https://en.wikipedia.org/wiki/Ford_Model_T"),
+        Event(description: "Girl Scouts are formed", year: 1912, month: 3, website: "https://en.wikipedia.org/wiki/Girl_Scouts_of_the_USA"),
+        Event(description: "Mickey Mouse's first appearance", year: 1928, month: 11, website: "https://en.wikipedia.org/wiki/Mickey_Mouse"),
+        Event(description: "Golden Gate Bridge completed in San Francisco", year: 1937, month: 4, website: "https://en.wikipedia.org/wiki/Golden_Gate_Bridge"),
+        Event(description: "Winston Churchill's Iron Curtain speech", year: 1946, month: 3, website: "https://en.wikipedia.org/wiki/Iron_Curtain"),
+        Event(description: "The comic strip Peanuts is first published", year: 1950, month: 10, website: "https://en.wikipedia.org/wiki/Peanuts"),
+        Event(description: "The debut of the Today show on NBC", year: 1952, month: 1, website: "https://en.wikipedia.org/wiki/Today_(U.S._TV_program)"),
+        Event(description: "Disneyland opens in California", year: 1955, month: 7, website: "https://en.wikipedia.org/wiki/Disneyland"),
+        Event(description: "Sesame Street premieres on TV", year: 1969, month: 11, website: "https://en.wikipedia.org/wiki/Sesame_Street"),
+        Event(description: "The first Earth Day is observed", year: 1970, month: 4, website: "https://en.wikipedia.org/wiki/Earth_Day")
     ]
     
     var events: [Event] = []
